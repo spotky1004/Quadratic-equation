0f13a5a839ad0fc80d2e8550d2d3700f7cebf2aa
Spotky1004 <spotky1004@gmail.com>
2020-05-21 17:40:03 +0900

    0.12

--- index.js
+++ index.js
@@ -11,27 +11,27 @@ $(function (){
     checkFrom = (b**2)-(4*a*c);
     if (a == 0 && b == 0 && c == 0) {
       root1 = 0;
-      outputStr = 'x = ' + root1.toFixed(3);
+      outputStr = 'x = ' + root1.toFixed(5);
     } else if (a == 0) {
       if (b != 0) {
         root1 = -1*(c/b);
-        outputStr = 'x = ' + root1.toFixed(3);
+        outputStr = 'x = ' + root1.toFixed(5);
       } else {
         root1 = 0;
-        outputStr = 'x = ' + root1.toFixed(3);
+        outputStr = 'x = ' + root1.toFixed(5);
       }
     } else if (checkFrom >= 0) {
       root1 = (-b+Math.sqrt(checkFrom))/(2*a);
       root2 = (-b+Math.sqrt(checkFrom))/(2*a);
       if (root1 != root2) {
-        outputStr = 'x = ' + root1.toFixed(3) + ' or x = ' + root2.toFixed(3);
+        outputStr = 'x = ' + root1.toFixed(5) + ' or x = ' + root2.toFixed(5);
       } else {
-        outputStr = 'x = ' + root1.toFixed(3) + '(multiple root)'
+        outputStr = 'x = ' + root1.toFixed(5) + '(multiple root)'
       }
     } else {
       rootc = (-b)/(2*a);
       rooti = Math.sqrt(Math.abs(checkFrom))/((2*a));
-      outputStr = 'x = ' + rootc.toFixed(3) + '+' + rooti.toFixed(3) + 'i or x = ' + rootc.toFixed(3) + (-rooti).toFixed(3) + 'i';
+      outputStr = 'x = ' + rootc.toFixed(5) + '+' + rooti.toFixed(5) + 'i or x = ' + rootc.toFixed(5) + (-rooti).toFixed(5) + 'i';
     }
     $('#rootWarp').html(function (index,html) {
       return outputStr;
