a944b562c2a2d6f992fb89c1c0f36154ee1605d6
Spotky1004 <spotky1004@gmail.com>
2020-05-21 11:50:33 +0900

    0

---
+++ index.js
@@ -0,0 +1,23 @@
+$(function (){
+  $('input').keypress(function (event) {
+    if (event.which && (event.which <= 47 || event.which >= 58) && event.which != 8) {
+      event.preventDefault();
+    }
+    setTimeout( function (){
+      a = Number($('input#const3').val());
+      b = Number($('input#const2').val());
+      c = Number($('input#const1').val());
+      checkFrom = (b**2)-(4*a*c);
+      if (checkFrom >= 0) {
+        root1 = (-b+Math.sqrt(checkFrom))/2*a;
+        root2 = (-b+Math.sqrt(checkFrom))/2*a;
+        outputStr = 'x = ' + root1.toFixed(3) + ' or x = ' + root2.toFixed(3);
+      } else {
+        outputStr = 'Has no root!';
+      }
+      $('#rootWarp').html(function (index,html) {
+        return outputStr;
+      });
+    }, 0);
+  });
+});
